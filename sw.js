264b3724a142727c28cd1d7bf1ffda1e6843eda5
Matheus Oliveira <contato.oliveiramaker@gmail.com>
2026-09-22 06:51:36 -0300

    fix: atualizar cache offline e incluir icones PWA

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'metas-cache-v1';
+const CACHE_NAME = 'bolso-cache-v2';
 const FILES_TO_CACHE = [
   './',
   './index.html',
@@ -16,5 +16,14 @@ self.addEventListener('fetch', event => {
     caches.match(event.request).then(response => {
       return response || fetch(event.request);
     })
+  );
+});
+
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    ))
   );
 });
